996cc7de92121d221ffd211e437e3a588ae15ea0
Kartik-Garg <kartikgarg938@gmail.com>
2022-09-16 12:20:33 +0530

    modifications for user to change any field

--- task1/main.go
+++ task1/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Profile struct {
 	Name           string
@@ -21,9 +23,65 @@ func completeProfile(p Profile) Profile {
 }
 
 // method to take profile and modify it by taking modifications as arguments
-func (p Profile) updateProfile(modifications string) Profile {
-	//modifying only the name here
-	p.Name = modifications
+func (p Profile) updateProfile(modifications int) Profile {
+	//writing switch case for modifications
+	switch modifications {
+
+	case 1:
+		fmt.Printf("Please enter the new name to be modified:")
+		var name string
+		fmt.Scanln(name)
+		p.Name = name
+		return p
+	case 2:
+		fmt.Println("Please enter the new username to be modified:")
+		var username string
+		fmt.Scan(&username)
+		p.Username = username
+		return p
+	case 3:
+		fmt.Println("Please enter the new designation to be modified:")
+		var designation string
+		fmt.Scan(&designation)
+		p.Designation = designation
+		return p
+	case 4:
+		fmt.Println("Enter new contact number to be modified:")
+		var contact string
+		fmt.Scan(&contact)
+		p.ContactNumber = contact
+		return p
+	case 5:
+		fmt.Println("Press 1 to make changes in image name\nPress 2 to make changes in image path\nPress 3 to make changes in both image name and image path: ")
+		var profile_changes int
+		fmt.Scan(&profile_changes)
+		if profile_changes == 1 {
+			fmt.Println("Enter the new image name:")
+			var image_name string
+			fmt.Scan(&image_name)
+			p.ProfilePicture.ImageName = image_name
+			return p
+		}
+		if profile_changes == 2 {
+			fmt.Println("Enter the new image path:")
+			var image_path string
+			fmt.Scan(&image_path)
+			p.ProfilePicture.ImagePath = image_path
+			return p
+		}
+		if profile_changes == 3 {
+			var image_name, image_path string
+			fmt.Println("Enter the new image name:")
+			fmt.Scan(&image_name)
+			fmt.Println("Enter the new image path:")
+			fmt.Scan(&image_path)
+			p.ProfilePicture.ImageName = image_name
+			p.ProfilePicture.ImagePath = image_path
+			return p
+		}
+
+	}
+	//returning profile
 	return p
 }
 
@@ -41,5 +99,11 @@ func main() {
 
 	fmt.Println(completeProfile(p))
 
-	fmt.Println(p.updateProfile("andrew"))
+	//taking input from user for modification
+	var num int
+	fmt.Println("Please enter the number in respect with modifications to be made:\n 1.Name\n 2.Username\n 3.Designation\n 4.ContactNumber\n 5.ProfilePicture")
+	fmt.Scanln(&num)
+
+	//output after modifications
+	fmt.Println("Profile after modificatoin is: ", p.updateProfile(num))
 }
