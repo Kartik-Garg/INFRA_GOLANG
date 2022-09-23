5e5a52d7454e96d35d83c0502f5699cf199f6a85
Kartik-Garg <kartikgarg938@gmail.com>
2022-09-23 12:57:56 +0530

    removing unnecessary comments from task1

--- task1/main.go
+++ task1/main.go
@@ -29,20 +29,6 @@ func completeProfile(name, userName, designation, contactNumber, imageName, imag
 	}
 }
 
-// func completeProfile(name, userName, designation, contactNumber, imageName, imagePath string) Profile {
-// 	//using pointer return type so the changes do not remain local to this specific function, pointers pass the actual address of the type rather than making a copy of it
-// 	return Profile{
-// 		Name:          name,
-// 		Username:      userName,
-// 		Designation:   designation,
-// 		ContactNumber: contactNumber,
-// 		ProfilePicture: ProfilePicture{
-// 			ImageName: imageName,
-// 			ImagePath: imagePath,
-// 		},
-// 	}
-// }
-
 // method to update profile which takes all the modifications
 func (p *Profile) UpdateProfile(name, userName, designation, contactNumber, imageName, imagePath string) {
 	//taking a pointer here as well so the changes are made to actual profile rather than the local copy of this particular method
@@ -66,29 +52,6 @@ func (p *Profile) UpdateProfile(name, userName, designation, contactNumber, imag
 	}
 
 }
-
-// func (p Profile) UpdateProfile(name, userName, designation, contactNumber, imageName, imagePath string) {
-// 	//taking a pointer here as well so the changes are made to actual profile rather than the local copy of this particular method
-// 	if name != "" {
-// 		p.Name = name
-// 	}
-// 	if userName != "" {
-// 		p.Username = userName
-// 	}
-// 	if designation != "" {
-// 		p.Designation = designation
-// 	}
-// 	if contactNumber != "" {
-// 		p.ContactNumber = contactNumber
-// 	}
-// 	if imageName != "" {
-// 		p.ProfilePicture.ImageName = imageName
-// 	}
-// 	if imagePath != "" {
-// 		p.ProfilePicture.ImagePath = imagePath
-// 	}
-
-// }
 
 func main() {
 	name := "Kartik"
