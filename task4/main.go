2029b4112054bb05e3485954846d017cec068c51
Kartik-Garg <kartikgarg938@gmail.com>
2022-09-23 12:53:10 +0530

    made changes in channel names

--- task4/main.go
+++ task4/main.go
@@ -4,25 +4,25 @@ import (
 	"fmt"
 )
 
-func iterateOverArray(s []string, chanel1 chan []string) {
+func iterateOverArray(s []string, stringArrayChannel chan []string) {
 	var words []string
 	//make empty channel here and get data from inner most function
-	chanel2 := make(chan string)
+	wordsChannel := make(chan string)
 
 	for _, k := range s {
 		//send word and empty channel
-		go removeNum(k, chanel2)
-		//receive chanel2 here and add to array
+		go removeNum(k, wordsChannel)
+		//receive wordsChannel here and add to array
 	}
 
 	for i := 0; i < len(s); i++ {
-		words = append(words, <-chanel2)
+		words = append(words, <-wordsChannel)
 	}
 	//send the words array back to outer func
-	chanel1 <- words
+	stringArrayChannel <- words
 }
 
-func removeNum(s string, chanel2 chan string) {
+func removeNum(s string, wordsChannel chan string) {
 
 	//we receive each word here
 	var k string
@@ -34,19 +34,19 @@ func removeNum(s string, chanel2 chan string) {
 		}
 
 	}
-	//got one word, feed it to the internal (chanel2)
-	chanel2 <- k
+	//got one word, feed it to the internal (wordsChannel)
+	wordsChannel <- k
 }
 
 func main() {
 	arr := []string{"gopher123", "alpha99beta", "1cita2del3"}
 
-	chanel1 := make(chan []string)
-	defer close(chanel1)
+	stringArrayChannel := make(chan []string)
+	defer close(stringArrayChannel)
 
 	//passed array and empty channel
-	go iterateOverArray(arr, chanel1)
-	//receive from the chanel1 and print
-	fmt.Println(<-chanel1)
+	go iterateOverArray(arr, stringArrayChannel)
+	//receive from the stringArrayChannel and print
+	fmt.Println(<-stringArrayChannel)
 
 }
